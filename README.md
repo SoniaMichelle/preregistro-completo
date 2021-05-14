ba94fa3b245551ccf69fa989827e17ae40017e54
SoniaMichelle <gonzalezortizmichelle@gmail.com>
2021-05-14 11:46:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# preregistro-completo
+ 
---
+++ app/config.php
@@ -0,0 +1,11 @@
+<?php
+
+  // construccion de constantes PHP para contener las rutas de la JSP
+  
+  define('NOMBRE_SERVIDOR', 'itmilpaalta2');
+
+  define('SERVIDOR', 'http://localhost/preregistro/');
+  //define('SERVIDOR', 'http://localhost/preregistro/');
+
+
+?>
